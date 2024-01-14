960b4be0ef38e9ea04039045838678053b9a8171
Cuong Quoc Dang <84326038+cuongquocdang@users.noreply.github.com>
2024-01-14 10:01:25 +0700

    feat(http-client): apply RestClient

--- src/main/java/com/example/microservice/HttpClientConfig.java
+++ src/main/java/com/example/microservice/HttpClientConfig.java
@@ -2,25 +2,26 @@ package com.example.microservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import org.zalando.logbook.Logbook;
-import org.zalando.logbook.spring.webflux.LogbookExchangeFilterFunction;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @Configuration
 public class HttpClientConfig {
 
     @Bean
-    public TestClient testClient(final WebClient.Builder webClientBuilder, final Logbook logbook) {
+    public TestClient testClient(final RestClient.Builder restClientBuilder,
+                                 final Logbook logbook) {
 
-        var webClient = webClientBuilder
+        var webClient = restClientBuilder
                 .baseUrl("https://jsonplaceholder.typicode.com")
-                .filter(new LogbookExchangeFilterFunction(logbook))
+                .requestInterceptor(new LogbookClientHttpRequestInterceptor(logbook))
                 .build();
 
         var httpServiceProxyFactory =
-                HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient))
+                HttpServiceProxyFactory.builderFor(RestClientAdapter.create(webClient))
                         .build();
 
         return httpServiceProxyFactory.createClient(TestClient.class);
